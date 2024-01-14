40df748acb2044bea6317156fa6df5f16279b0bd
dustyou <1172073068@qq.com>
2024-01-15 00:10:43 +0800

    'auto'

--- transformer/test/torch_test.py
+++ transformer/test/torch_test.py
@@ -3,23 +3,24 @@ from torch import nn
 
 # 01
 # randn
-# x = torch.randn(4,4)
-# print(x.size())
-# y = x.view(2,2,2,2)
-# print(y.size())
-# a = torch.randn(1,2,3,4)
-# print(a.size(), a)
-# b = a.transpose(1,2)
-# print(b.size(), b)
-# c = a.view(1,3,2,4)
-# print(c.size(), c)
+x = torch.randn(4,4)
+print(x.size())
+# view
+y = x.view(2,2,2,2)
+print(y.size())
+a = torch.randn(1,2,3,4)
+print(a.size(), a)
+# transpose
+b = a.transpose(1,2)
+print(b.size(), b)
+# transpose和view
+c = a.view(1,3,2,4)
+print(c.size(), c)
 
 # # embedding
 embedding = nn.Embedding(10, 3)
 input = torch.LongTensor([[1,2,4,5],[4,3,2,9]])
 print(embedding(input))
-print(embedding.shape)
-
 
 # nn.Dropout演示
 m=nn.Dropout(p=0.2)
