d40c0cfece1ba5e93b773522465fe196e9d203ee
dustyou <1172073068@qq.com>
2024-01-22 23:35:55 +0800

    'auto'

--- transformer/test/embedding_test.py
+++ transformer/test/embedding_test.py
@@ -3,5 +3,5 @@ from torch import nn
 
 # # embedding
 embedding = nn.Embedding(10, 3)
-input = torch.LongTensor([[1,2,4,5],[4,3,2,9]])
+input = torch.LongTensor([[1,2,4,5,5],[4,3,2,9,8]])
 print(embedding(input))
