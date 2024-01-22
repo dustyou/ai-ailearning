f18494cd43a028da99e211e972773c1e6cc316f3
dustyou <1172073068@qq.com>
2024-01-22 22:17:17 +0800

    'auto'

---
+++ transformer_02/code/RNN_code/demo_test.py
@@ -0,0 +1,10 @@
+import nn
+import torch
+
+embedding = nn.Embedding(10, 3)
+input1 = torch.LongTensor([[1,2,4,5],[4,3,2,9]])
+print(embedding(input1))
+
+embedding = nn.Embedding(10, 3, padding_idx=0)
+input1 = torch.LongTensor([[0, 2, 3, 5]])
+print(embedding(input1))
